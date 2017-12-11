36e177028c5f40db167b1330f320e89db17dd5e7
Ben Spiers <ben.spiers22@gmail.com>
2017-12-11 23:03:09 +0000

    Oops, commit header change for changed function signature

--- src/graphics/window.h
+++ src/graphics/window.h
@@ -28,4 +28,4 @@ int setupMatrix(double sourceDistance, double detectorDistance, double openingAn
 int setupBuffers(double pos[], bool running[], long N);
 int updateBuffers(double pos[], bool running[], long N);
 
-int draw(int N, bool wait = false);
+int draw(int N, int name_length, char* name, bool wait = false);
